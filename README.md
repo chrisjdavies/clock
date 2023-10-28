2aa907f2ddf5e06f20cc990cc842bab35e6dc96d
Chris <chrisjdavies@users.noreply.github.com>
2023-10-28 20:53:05 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Super Simple Clock for VS Code
+
+![Clock](images/icon.png)
+
+A super simple, open source status-bar clock for Visual Studio Coe.
+
+There seem to be lots of status bar clock extensions for Visual Studio Code, but
+nearly all of them are closed-source?  Something so simple should surely be free
+and open -- so here we go.
+
+## Configuration
+
+|Property|Description|Type|Default value|
+|---|---|---|---|
+|`clock.format24Hour`|Display a 24-hour clock.|Boolean|`true`|
+
+## Thanks
+
+- Thanks to DALL-E 3 for generating the icon/logo.
---
+++ src/extension.ts
@@ -0,0 +1,37 @@
+import * as vscode from 'vscode';
+
+const DEFAULT_24_HOUR = true;
+const DEFAULT_UPDATE_PERIOD = 1000;
+
+let statusItem: undefined | vscode.StatusBarItem;
+let show24Hour: boolean = DEFAULT_24_HOUR;
+
+function tick() {
+	if (statusItem) {
+		const config = vscode.workspace.getConfiguration('clock');
+		const cfg24Hour = config.get<boolean>('format24Hour');
+		if (cfg24Hour === undefined) {
+			show24Hour = DEFAULT_24_HOUR;
+		} else {
+			show24Hour = cfg24Hour;
+		}
+
+		statusItem.text = new Date().toLocaleTimeString(vscode.env.language, { hour12: !show24Hour, timeStyle: 'short' });
+		setTimeout(tick, DEFAULT_UPDATE_PERIOD);
+	}
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	statusItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, -1337);
+	tick();
+
+	statusItem.show();
+}
+
+export function deactivate() {
+	if (statusItem) {
+		statusItem.hide();
+		statusItem.dispose();
+		statusItem = undefined;
+	}
+}
